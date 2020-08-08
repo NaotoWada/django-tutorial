5fa734cff6d84328bd5e2b7610757bd8a1f0ab16
NaotoWada <naoto.oct25@gmail.com>
2020-08-08 13:42:53 +0900

    delete create init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,1 @@
-DROP DATABASE IF EXISTS tutorial;
-DROP DATABASE IF EXISTS test_tutorial;
-CREATE DATABASE tutorial;
-CREATE DATABASE test_tutorial;
 GRANT ALL ON tutorial.* TO user;
-GRANT ALL ON test_tutorial.* TO user;
