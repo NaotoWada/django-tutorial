ff0061bedac3caaa81d48ab7526de187e84cbcf3
NaotoWada <naoto.oct25@gmail.com>
2020-08-08 13:32:39 +0900

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": "tutorial",
         "USER": "user",
         "PASSWORD": "user",
-        "HOST": "db",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
         "TEST": {"NAME": "test_tutorial",},
     }
